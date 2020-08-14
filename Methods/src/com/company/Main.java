6bbe5e6216a0018724620aec5a06cf9d41438c75
Oleh Martyniv <oleh.martyniv@gmail.com>
2020-08-14 10:30:10 +0300

    methods added

---
+++ Methods/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+        displayHighScorePosition("Tim", 1500);
+        displayHighScorePosition("Oleh", 900);
+        displayHighScorePosition("Henri", 400);
+        displayHighScorePosition("Sarah", 50);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + levelCompleted * bonus;
+            finalScore += 2000;
+            return finalScore;
+        }
+
+        return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed to get into position " + calculateHighScorePosition(position)
+                + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
