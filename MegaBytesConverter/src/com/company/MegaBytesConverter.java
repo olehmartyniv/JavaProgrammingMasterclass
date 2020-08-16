12e3b24683fbb42dde97da64a9f5d100ebc3d0ce
Oleh Martyniv <oleh.martyniv@gmail.com>
2020-08-16 22:15:34 +0300

    MegaBytes Converter

---
+++ MegaBytesConverter/src/com/company/MegaBytesConverter.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + Math.round(kiloBytes / 1024) + " MB and " + kiloBytes % 1024
+                    + " KB");
+        }
+    }
+}
--- SpeedConverter/src/com/company/Main.java
+++ SpeedConverter/src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        SpeedConverter.printConversion(1.5);
+        SpeedConverter.printConversion(25.42);
     }
 }
