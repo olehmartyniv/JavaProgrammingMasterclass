6180a2d2fb5d64e1a8ca791394f44a1b69819b2a
Oleh Martyniv <oleh.martyniv@gmail.com>
2020-09-05 17:06:20 +0300

    Inheritance

--- Inheritance/src/com/company/Main.java
+++ Inheritance/src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
+	    Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+	    Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
+	    dog.eat();
+	    dog.walk();
+	    dog.run();
+
+	    Fish fish = new Fish("Shark", 25, 100, 2, 2, 3);
+	    fish.swim(15);
     }
 }
