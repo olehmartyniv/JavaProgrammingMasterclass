be8a92fb89c93da5ee16e106a1e6366110cfdc84
Oleh Martyniv <oleh.martyniv@gmail.com>
2020-09-10 08:54:10 +0300

    Inheritance Challenge

---
+++ InheritanceChallenge/src/com/company/Audi.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Audi extends Car {
+
+    private int roadServiceMonths;
+
+    public Audi(int roadServiceMonths) {
+        super("Audi", "4WD", 5, 5, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+    public void accelerate(int rate) {
+        int newVelocity = getCurrentVelocity() + rate;
+        if (newVelocity == 0) {
+            stop();
+            changeGear(1);
+        } else if (newVelocity > 0 && newVelocity <= 10) {
+            changeGear(1);
+        } else if (newVelocity > 10 && newVelocity <= 20) {
+            changeGear(2);
+        } else if (newVelocity > 30 && newVelocity <= 40) {
+            changeGear(3);
+        } else {
+            changeGear(4);
+        }
+
+        if (newVelocity > 0) {
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+    }
+}
