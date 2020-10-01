36310d308256c8adc72f1f76d089b10806a53856
Oleh Martyniv <oleh.martyniv@gmail.com>
2020-10-02 00:57:43 +0300

    added logic in main

--- AutoboxingAndUnboxingChallenge/src/com/company/Bank.java
+++ AutoboxingAndUnboxingChallenge/src/com/company/Bank.java
@@ -4,13 +4,32 @@ import java.util.ArrayList;
 
 public class Bank {
 
+    private String bankName;
     private ArrayList<Branch> branches = new ArrayList<>();
+
+    public Branch getBranch(String branchName) {
+        return branches.get(getIndexIfExist(branchName));
+    }
 
     public void createNewBranch(String branchName) {
         if (getIndexIfExist(branchName) >= 0) {
             System.out.println("Such branch is already exist");
+        } else if (branchName.isEmpty()) {
+            System.out.println("Name can't be blank");
         } else {
             branches.add(new Branch(branchName));
+        }
+    }
+
+    public void printBranchesList() {
+        if (branches.isEmpty()) {
+            System.out.println("There are no any branches");
+            return;
+        }
+
+        System.out.printf("%-3s %-10s\n", "#", "Name");
+        for (int i = 0; i < branches.size(); i++) {
+            System.out.printf("%-3s %-10s%n", (i + 1), branches.get(i).getBranchName());
         }
     }
 
--- AutoboxingAndUnboxingChallenge/src/com/company/Main.java
+++ AutoboxingAndUnboxingChallenge/src/com/company/Main.java
@@ -1,11 +1,65 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+	private static Scanner scanner = new Scanner(System.in);
+	private static Bank bank = new Bank();
+
     public static void main(String[] args) {
-	    Branch first = new Branch("qwe");
-	    first.createNewCustomer("Tim", 200);
-        first.addTransaction("Tim", 100);
-	    first.showBranchCustomers();
+		boolean showMenu = true;
+		while (showMenu) {
+			System.out.print("\nPress\n" +
+					"\t1 - To print the list of branches\n" +
+					"\t2 - To select branch\n" +
+					"\t3 - To add a new branch\n" +
+					"\t0 - To quit the application\n" +
+					"Enter you choice: ");
+			switch (scanner.nextLine()) {
+				case "0":
+					showMenu = false;
+					break;
+				case "1":
+					bank.printBranchesList();
+					break;
+				case "2":
+					System.out.print("Select a branch");
+					bank.printBranchesList();
+					branchMenu(scanner.nextLine());
+					break;
+				case "3":
+					System.out.print("Enter branch name: ");
+					bank.createNewBranch(scanner.nextLine());
+					break;
+			}
+		}
     }
+
+	public static void branchMenu(String branchName) {
+		Branch selectedBranch = bank.getBranch(branchName);
+		boolean showMenu = true;
+		while (showMenu) {
+			System.out.print("\nPress\n" +
+					"\t1 - To print the list of customers\n" +
+					"\t2 - To add a new customer\n" +
+					"\t3 - To add a new transaction\n" +
+					"\t0 - To go back\n" +
+					"Enter you choice: ");
+			switch (scanner.nextLine()) {
+				case "0":
+					showMenu = false;
+					break;
+				case "1":
+					selectedBranch.showBranchCustomers();
+					break;
+				case "2":
+					selectedBranch.createNewCustomer();
+					break;
+				case "3":
+
+					break;
+			}
+		}
+	}
 }
